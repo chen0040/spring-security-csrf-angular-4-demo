408757af90336f59834d40e70e389be26de0287c
xschen <xs0040@gmail.com>
2017-12-08 19:36:22 +0800

    js client to spring security

--- spring-boot-js-client/src/sbjclient.js
+++ spring-boot-js-client/src/sbjclient.js
@@ -1,31 +1,90 @@
 var jssb = jssb || {};
 
+if(require){
+    axios = require('axios');
+}
+
 (function (jss) {
 
-    jss.copyArray = function(a) {
-        var b = [];
-        for (var i = 0; i < a.length; ++i) {
-            b.push(a[i]);
-        }
-        return b;
-    };
 
-    jss.copyMap = function(a) {
-        var b = {};
-        for (var key in a) {
-            b[key] = a[key];
-        }
-        return b;
-    };
 
     var SpringBootClient = function() {
-        this.token = null;
+        this._csrf = null;
         this.sessionId = null;
+        this.authenticated = false;
     };
 
-    SpringBootClient.prototype.login = function (url, username, password) {
+    SpringBootClient.prototype.login = function (url, username, password, callback) {
+        this.loginGet(url, (function(_username, _password){
+            return function(_url){
+                  axios({
+                      method: 'post',
+                      url: _url,
+                      responseType: 'json',
+                      data: {
+                          username: _username,
+                          password: _password
+                      },
+                      headers: {
+                          '_csrf': this._csrf,
+                          'Cookie': 'XSRF-TOKEN=' + this._csrf,
+                          'X-XSRF-TOKEN': this._csrf
+            }
+                  }).then(function(response){
+                      var result = response.data;
+                      this.authenticated = result.authenticated;
+                      if(result.authenticated) {
+                          var cookie = response.headers['set-cookie'][0];
+                          var cookies = cookie.split(';');
+                          for(var i=0; i < cookies.length; ++i){
+                              var c = cookies[i];
+                              var cpair = c.split('=');
+                              if(cpair.length === 2) {
+                                  var name = cpair[0];
+                                  var value = cpair[1];
+                                  if(name === 'JSESSIONID') {
+                                      this.sessionId = value;
+                                      callback(this._csrf, this.sessionId, this.authenticated);
+                                      break;
+                                  }
+                              }
+                          }
+                      } else {
 
+                          callback('', '', false);
+                      }
+                  });
+            };
+        })(username, password));
     };
+
+    SpringBootClient.prototype.loginGet = function(url, callback) {
+        axios({
+            method:'get',
+            url:url,
+            responseType:'json'
+        })
+            .then(function(response) {
+
+                var cookie = response.headers['set-cookie'][0];
+                var cookies = cookie.split(';');
+                for(var i=0; i < cookies.length; ++i){
+                    var c = cookies[i];
+                    var cpair = c.split('=');
+                    if(cpair.length === 2) {
+                        var name = cpair[0];
+                        var value = cpair[1];
+                        if(name === 'XSRF-TOKEN') {
+                            this._csrf = value;
+                            callback(url);
+                            break;
+                        }
+                    }
+                }
+            });
+    };
+
+
 
     jss.SpringBootClient = SpringBootClient;
 
---
+++ spring-boot-js-client/test/sbjclient-spec.js
@@ -0,0 +1,32 @@
+var expect    = require("chai").expect;
+var sbjclient = require('../src/sbjclient');
+
+describe("Spring Boot Login api test", function() {
+  describe("can obtain token and session id if authenticated", function() {
+    var url = 'http://localhost:8080/erp/login-api-json';
+
+    var cl = new sbjclient.SpringBootClient();
+    cl.login(url, "admin", "admin", function(_csrf, _sessionId, authenticated){
+        console.log('authenticated: ' + authenticated);
+        console.log('_csrf: ' + _csrf);
+        console.log('JSESSIONID: ' + _sessionId);
+        expect(authenticated).to.equal(true);
+    });
+
+  });
+
+
+    describe("cannot obtain token and session id if failed", function() {
+        var url = 'http://localhost:8080/erp/login-api-json';
+
+        var cl = new sbjclient.SpringBootClient();
+        cl.login(url, "admin", "wrong-password", function(_csrf, _sessionId, authenticated){
+            console.log('authenticated: ' + authenticated);
+            console.log('_csrf: ' + _csrf);
+            console.log('JSESSIONID: ' + _sessionId);
+            expect(authenticated).to.equal(false);
+        });
+
+    });
+
+});
